ba14a3a499929b227258bdd0edca69d3c2a7a310
SWADHIN SATYAPRAKASH MAJHI <95016250+swadhin1237@users.noreply.github.com>
2022-11-06 20:39:22 +0530

    major design change

---
+++ src/Component/Centre_portion.js
@@ -0,0 +1,189 @@
+import React from 'react'
+
+export default function Center() {
+  return (
+   
+    <div class="container text-center">
+    <div class="row">
+      <div class="col">
+      <div>
+        <div className="card" style={{width : 200 ,height : 475}} >
+    <img src="Heritag.png" className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Heritage</h5>
+      <p className="card-text">multicuisine, chinese, south indian, north indian, continental #great taste and healthy food.</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">IIT Campus, IIT Kharagpur, Kharagpur, West Bengal · - 1 km</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+    </div>
+  </div>
+  <br></br>
+  <div className="card" style={{width : 200,height : 475}} >
+    <img src="foodstudio.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Food Studio</h5>
+      <p className="card-text">Food Studio is a name associated with grand and passionate world of flavours.</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">IIT Campus, IIT Kharagpur, Kharagpur, West Bengal · - 1 km</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+  
+    </div>
+  </div>
+      </div>
+      </div>
+      <div class="col">
+      <div>
+        <div className="card" style={{width : 200,height : 475}} >
+    <img src="smartpind.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">SMART PIND</h5>
+      <p className="card-text">North Indian restaurant </p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">IIT Avenue Road, near Cafe Coffee Day, Scholar Canvas Stadium, IIT Kharagpur
+  </li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+     
+    </div>
+   
+  </div>
+   <br></br>
+  <div className="card" style={{width : 200,height : 475}} >
+    <img src="PFC.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">P F C</h5>
+      <p className="card-text">A good place to have food only thing is service is bit delayed here</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">Shop No. Oval 3, Inside IIT Kharagpur Campus, Near Patel Hall, Pan Loop</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+     
+    </div>
+  </div>
+      </div>
+      </div>
+      <div class="col">
+      <div>
+      <div className="card" style={{width : 200,height : 475}} >
+    <img src="subway.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Subway</h5>
+      <p className="card-text">Casual counter-serve chain for build-your-own sandwiches & salads, with health-conscious options.</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">Nalanda Complex, Ardeshir Dalal Ave, IIT Kharagpur</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+  
+    </div>
+   
+  </div>
+   <br></br>
+  <div className="card" style={{width : 200,height : 475}} >
+    <img src="foodcapital.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Food Capital</h5>
+      <p className="card-text">Good to eat with friends in a group</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">PURIGATE, opposite IIT KHARAGPUR, MAIN GATE</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+      
+    </div>
+  </div>
+      </div>
+      </div>
+      <div class="col">
+      <div>
+      <div className="card" style={{width : 200,height : 475}} >
+    <img src="vegiee.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Vegies restaurant</h5>
+      <p className="card-text">Service is good as their staff is very good and ambiance is also very nice</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">IIT Campus, IIT Kharagpur, Kharagpur, West Bengal -- 1km</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+     
+    </div>
+  </div>
+  <br></br>
+  
+  <div className="card" style={{width : 200,height : 475}} >
+    <img src="amigo.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Amigo</h5>
+      <p className="card-text">Awesome place for chicken Biryani and other non veg dishes. </p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">IIT KGP Rail Over Bridge, Kharagpur, West Bengal 721301</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu </a>
+  
+    </div>
+  </div>
+      </div>
+      </div>
+      <div class="col">
+      <div>
+      <div className="card" style={{width : 200,height : 475}} >
+    <img src="foodcity.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Food City</h5>
+      <p className="card-text">Serve a large variety of dishes from Chinese to south Indian. Generally there is a little waiting time and food is pocket friendly.</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">Shop No. RKH-1, IIT Opp. R.K.Hall, Kharagpur</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+    </div>
+  </div>
+   <br></br>
+  <div className="card" style={{width : 200,height : 475}} >
+    <img src="prince.png" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">The Prince Restaurant</h5>
+      <p className="card-text">Pricing is moderately high, but excellent quality and variety of food!</p>
+    </div>
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">Location</li>
+      <li className="list-group-item">SH 5, Prembazar, Kharagpur, West Bengal 721306</li>
+    </ul>
+    <div className="card-body">
+      <a href="/" className="card-link">Menu</a>
+    </div>
+  </div>
+      </div>
+      </div>
+    </div>
+  </div>
+
+  
+  )
+}
